e1d56802a4661a65ae746f85580f26dea92dd3f8
Yinghao Wang <wangyinghao241@gmail.com>
2023-07-11 18:39:28 +0200

    -front-end development

--- bmc/urls.py
+++ bmc/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'bmc'
 urlpatterns = [
-    path('search/', views.search, name='search'),
-    path('search/afterselect/', views.after_select, name='afterselect'),
+    path('', views.welcome, name='welcome'),
+    path('<int:journal>/search/', views.search, name='search'),
+    path('<int:journal>/search/afterselect/', views.after_select, name='afterselect'),
     path('search/result/', views.result, name='result'),
     path('search/save/', views.save, name='save'),
     path('search/download/', views.download_pdf, name='download'),
